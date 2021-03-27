475e7ab309a43b6418c3eaad4c14ee94dc0fe773
Jorge Herrera <jorgearturo.herrerajimenez@gmail.com>
2021-03-26 20:53:20 -0500

    crear app react

---
+++ src/components/MiComponente.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MiComponente extends React.Component{
+    render(){
+        return(
+            <h1>Hola desde componente</h1>
+        );
+    }
+}
+
+export default MiComponente;
